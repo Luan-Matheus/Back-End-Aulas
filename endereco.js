9ec9b3c82be154c704ff251642acf67662202422
Luan-Matheus <pereira.luan25@escola.pr.gov.br>
2026-03-10 14:37:54 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,9 @@
+console.log("Endereço Completo")
+
+let pais = "Brasil"
+let estado = "Parana"
+let cidade = "Curitiba"
+
+let endereco = pais  + " - " + estado + " - " + cidade;
+//console.log(endereco);
+console.log("O endereço correto é: " + endereco);
