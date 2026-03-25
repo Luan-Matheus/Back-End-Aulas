dc8575ac8a66898931b3e85eb1e1a7a370c5b459
Luan-Matheus <pereira.luan25@escola.pr.gov.br>
2026-03-25 18:28:51 +0000

    versao1

---
+++ produtos.js
@@ -0,0 +1,9 @@
+const produto = "Mochila Impermeavel";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessório";
+
+console.log(`O produto é: ${produto}.`);
+console.log(`A ${produto}) custa R$ ${preco.toFixed}.`);
+console.log(`O estoque ainda restam ${estoque} mochilas.`);
+console.log(`A categoria de ${produto} é ${categoria}.`);
