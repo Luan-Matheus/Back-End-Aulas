fe6b0a02dd06cb8874f2cdf45c001392d8250ae6
Luan-Matheus <pereira.luan25@escola.pr.gov.br>
2026-03-25 18:48:08 +0000

    versao2

--- temperatura.js
+++ temperatura.js
@@ -1,5 +1,8 @@
 //Conversor de temperaturas
 const cidade = "Curitiba"
-let tempCelsius = 18.5;
+let tempCelsius = 26;
  const tempFahrenheit = tempCelsius * (9/5 ) + 32;
  const tempKelvin = tempCelsius + 273.15;
+ console.log(`A temperatura de ${cidade} está ${tempCelsius} Cº`);
+ console.log(`Fahrenheit: ${tempFahrenheit.toFixed(1)} Fº`);
+ console.log (`Kelvin: ${tempKelvin.toFixed(1)} Kº`);
