8cf59dcf9b2045c627587137e4308991f36cb87f
Luan-Matheus <pereira.luan25@escola.pr.gov.br>
2026-03-25 18:30:30 +0000

    versao1

--- produtos.js
+++ produtos.js
@@ -4,6 +4,6 @@ let estoque = 14;
 const categoria = "acessório";
 
 console.log(`O produto é: ${produto}.`);
-console.log(`A ${produto}) custa R$ ${preco.toFixed}.`);
+console.log(`A ${produto}) custa R$ ${preco.toFixed(2)}.`);
 console.log(`O estoque ainda restam ${estoque} mochilas.`);
 console.log(`A categoria de ${produto} é ${categoria}.`);
