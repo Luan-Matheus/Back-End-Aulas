1b611ed12b6674600d581d415af2947b65337780
Luan-Matheus <pereira.luan25@escola.pr.gov.br>
2026-03-31 17:36:22 +0000

    versao1

---
+++ operadores.js
@@ -0,0 +1,28 @@
+//OPERADOES E && (NOT)
+let maiorIdade = true;
+let possuiDocumento = false;
+
+if (maiorIdade && possuiDocumento) {
+    console.log ('Pode entrar na festa!')
+ } else {
+        console.log ('Não entrar na festa!')
+};
+ 
+//OPERADOR OU || (OR)
+let cartaoCredito = false;
+let dinheiro = true
+
+if (cartaoCredito || dinheiro) {
+    console.log('Pode fazer a compra.')
+} else {
+    console.log('Não pode fazer a compra a compra.')
+};
+
+//OPERADOR NÃO ! (NOT)
+let estaChovendo = true;
+
+if(!estaChovendo) {
+    console.log('Pode sair sem guarda-chuva.')
+} else {
+    console.log('Leve o guarda-chuva.')
+}
