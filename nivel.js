a66bd88e96c0ed882c5d5d5701c4437d6326ee0c
Luan-Matheus <pereira.luan25@escola.pr.gov.br>
2026-04-14 17:34:28 +0000

    v1

---
+++ nivel.js
@@ -0,0 +1,7 @@
+function subirDeNivel(nivelAtual) {
+        return nivelAtual + 1;
+        };
+
+        let meuNivel = 117;
+        let nivelNovo = subirDeNivel(meuNivel);
+        console.log("Seu nivel atual é: " + nivelNovo);
