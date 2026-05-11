e79b7088a91425820226d332e605de3e9054d5f2
Luan-Matheus <pereira.luan25@escola.pr.gov.br>
2026-05-11 19:11:19 +0000

    v1

---
+++ desafios.js
@@ -0,0 +1,11 @@
+const produto = "Celular A17 5G";
+let preco = 2000.00;
+let estoque = 14;
+const categoria = "Eletrônicos";
+const descricao = "Smartphone 252GB, Preto";
+
+console.log(`O produto é: ${produto}.`);
+console.log(`O ${produto} custa R$ ${preco.toFixed(2)}.`);
+console.log(`No estoque ainda restam ${estoque} unidades.`);
+console.log(`A categoria de ${produto} é ${categoria}.`);
+console.log(`Descrição: ${descricao}.`);
