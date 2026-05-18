b37a3c3c3760e1eadb63b222025c9d472bedb4a6
Luan-Matheus <pereira.luan25@escola.pr.gov.br>
2026-05-18 19:25:18 +0000

    versao2

--- desafios.js
+++ desafios.js
@@ -1,11 +1,23 @@
-const produto = "Celular A17 5G";
-let preco = 2000.00;
-let estoque = 14;
 const categoria = "Eletrônicos";
 const descricao = "Smartphone 252GB, Preto";
+const produto = "Smartphone";
+const preco = 2499.90;
+const estoque = 15;
 
 console.log(`O produto é: ${produto}.`);
 console.log(`O ${produto} custa R$ ${preco.toFixed(2)}.`);
 console.log(`No estoque ainda restam ${estoque} unidades.`);
 console.log(`A categoria de ${produto} é ${categoria}.`);
 console.log(`Descrição: ${descricao}.`);
+
+const numero1 = 20;
+const numero2 = 4;
+const resultadoSubtracao = numero1 - numero2;
+const resultadoMultiplicacao = numero1 * numero2;
+
+console.log(`O resultado da subtração é ${resultadoSubtracao}, e o resultado da multiplicação é ${resultadoMultiplicacao}.`);
+
+const desconto = 12;
+const precoComDesconto = preco * (1 - desconto / 100);
+
+console.log(`Preço com 12% de desconto: R$ ${precoComDesconto.toFixed(2)}.`);
